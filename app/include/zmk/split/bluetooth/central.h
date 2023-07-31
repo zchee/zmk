c9f90c6390c1092b3cf31186a6731863fe5e8cd6
David Dreschner <34408442+DerDreschner@users.noreply.github.com>
2023-07-31 11:37:10 +0200

    Fix broken CI builds on Zephyr 3.2 (#6)
    * fix(backlight/tests): Restore original behaviour to fix backlight tests
    
    * fix(build): Remove feature-specific code when SPLIT_BLE, RGB_UNDERGLOW or BACKLIGHT isn't enabled
    
    * refactor(bluetooth): Add battery reporting config.
    
    * Add dedicated battery reporting Kconfig that is `imply`d by
      enabling ZMK_BLE.
    
    * fix(rgb_underglow): Use correct condition in service.c
    
    * Revert "refactor(bluetooth): Add battery reporting config."
    
    This reverts commit 881da25a5affe141007f2c9fa938fc9d7f5753e5.
    
    ---------
    
    Co-authored-by: Peter Johanson <peter@peterjohanson.com>

--- app/include/zmk/split/bluetooth/central.h
+++ app/include/zmk/split/bluetooth/central.h
@@ -24,3 +24,10 @@ int zmk_split_bt_update_hid_indicator(zmk_hid_indicators_t indicators);
 int zmk_split_get_peripheral_battery_level(uint8_t source, uint8_t *level);
 
 #endif // IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
+#if IS_ENABLED(CONFIG_ZMK_RGB_UNDERGLOW)
+int zmk_split_bt_update_led(struct zmk_periph_led *periph);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_BACKLIGHT)
+int zmk_split_bt_update_bl(struct backlight_state *periph);
+#endif
